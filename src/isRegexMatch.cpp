6b10d0c753b096e0ff61f8ec3ead663d8d60ea49
Guoxin <guoxin@Confucius.(none)>
2013-10-02 12:17:33 -0500

    regexmatch

---
+++ src/isRegexMatch.cpp
@@ -0,0 +1,42 @@
+class RegularExpressionMatch
+{
+
+bool isMatch(const char* s, const char* p)
+{
+
+    unsigned int i = 0; // i is index for s
+    unsigned int j = 0; // j is index for p
+    char eos = '\0';
+    // I assume there is a '\0' at the end of each input string
+    //if (s[i]==eos || p[j]==eos ||p[j]=='*') return false; //error regex or empty input
+    while (p[j]!=eos)
+    {
+        char suffix = p[j+1];
+        if (suffix == '*')
+        {
+                while (s[i]==p[j]||p[j]=='.')
+                {
+                    if (isMatch(s+i, p+j+2))
+                        return true;
+                    else
+                        i++;
+                }
+                j+=2;
+        }
+        else
+        {
+            if (s[i]==p[j]||p[j]=='.')
+            {
+                i++; j++;
+            }
+            else
+                return false;
+        }
+        
+    }
+    if (p[j]==eos&&s[i]==eos)
+        return true;
+    else
+        return false;
+}
+};
