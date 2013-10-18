e5c2fb29d0964e4fb9e1d7cdaa03319bf075af4d
Guoxin <guoxin@Confucius.(none)>
2013-10-17 21:30:14 -0500

    add rotate matrix and stringproblems

--- include/CodingStudy.h
+++ include/CodingStudy.h
@@ -1,4 +1,8 @@
 namespace study
 {
     bool isMatch(const char*, const char*);
+    void removeDuplicateBitMap(string& str);
+    void removeDuplicateHashMap(string& str);
+    bool isAnagram(const string& str1, const string& str2);
+    void rotate90(uint32_t* im);
 }
